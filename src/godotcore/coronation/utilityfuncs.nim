60ed5291babe6bfd5414be217f1e52346be6f46f
la .panon. <panno.800k@gmail.com>
2024-07-17 23:35:23 +0900

    Prepare to implement UtilityFunctions

---
+++ src/godotcore/coronation/utilityfuncs.nim
@@ -0,0 +1,8 @@
+import godotcore/dirty/[ gdextension_interface ]
+export                   gdextension_interface
+
+import godotcore/[ builtinindex, commandindex, extracommands, methodtools]
+export             builtinindex, commandindex, extracommands, methodtools
+
+import godotcore/[ Variant, GodotClass ]
+export             Variant, GodotClass

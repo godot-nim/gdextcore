68709fec858c317aa0d5de6f6ea0d214413de48e
la .panon. <panno.800k@gmail.com>
2024-07-29 12:44:22 +0900

    Replace: eventindex to staticevents

--- src/godotcore/eventindex.nim
+++
@@ -1,8 +0,0 @@
-import events
-
-var init_engine* = (
-    on_load_builtinclassConstructor: event("load_builtinclassConstructor"),
-    on_load_builtinclassOperator: event("load_builtinclassOperator"),
-    on_load_builtinclassMethod: event("load_builtinclassMethod"),
-    on_load_metadata: event("load_metadata"),
-)
